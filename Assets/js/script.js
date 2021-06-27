28b4e4ea2a403c868b083746ff5b5ef58f67a438
David Szamek <davidszamek@gmail.com>
2021-06-27 19:16:38 +1000

    Updating javascript but also renamed the pathway in my directory from Assets to assets and so havent been updating alongside the index file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,54 @@
+var current = moment().hours();
 
-var button = $('.saveBtn')
+console.log(current);
 
-//Add current date at the top of the planner 
+// added a timer with AM/PM
+function update() {
+    $('#clock').html(moment().format('h:mm:ss A'));
+  }
+  
+  setInterval(update, 1000);
 
+  // console.log(update);
+
+// load the document html and css before third party
 $(document).ready(function () {
-  var date = moment().format('dddd MMMM Do');
 
-  $('#currentDay').text(date);
-});
+    // added current date to the top of the planner
+    var date = moment().format('dddd MMMM Do');
+
+    $('#currentDay').text(date);
+
+    console.log(date);
+
+
+    // adding past, present, future CSS classes 
+
+    function currentTime() {
+    // use var current from above 
+        var current = moment().hours();
+        console.log(current);
+
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id"));
+            console.log(current, blockTime)
+
+            if (blockTime < current) {
+                $(this).addClass("past");
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+            }
+            else if (blockTime === current) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }   
+        })
+    }
+    currentTime();
+})
