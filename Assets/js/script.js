9ba9858e6b8b0d65ffdee1292fe2e8fe44fba8e8
David Szamek <davidszamek@gmail.com>
2021-06-28 17:31:47 +1000

    added localStorage functionality and sample images

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,4 @@
+// define global variables
 var current = moment().hours();
 
 console.log(current);
@@ -15,17 +16,15 @@ function update() {
 $(document).ready(function () {
 
     // added current date to the top of the planner
-    var date = moment().format('dddd MMMM Do');
+    var date = moment().format('dddd, MMMM Do');
 
     $('#currentDay').text(date);
 
     console.log(date);
 
-
     // adding past, present, future CSS classes 
 
     function currentTime() {
-    // use var current from above 
         var current = moment().hours();
         console.log(current);
 
@@ -51,4 +50,31 @@ $(document).ready(function () {
         })
     }
     currentTime();
+    
+    // load any localStorage previously saved - # to get id
+    $("#8 .description").val(localStorage.getItem("8"));
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+    $("#18 .description").val(localStorage.getItem("18"));
+
+    // saving user input to localStorage on save btn click
+    // Inside the jQuery "this" will hold the original element selected (saveBtn - "click")
+    $(".saveBtn").on("click", function (event) {
+        event.preventDefault();
+        var time = $(this).parent().attr("id");
+        var reminder = $(this).siblings(".description").val();
+        
+        // Had this originally as localStorage.setItem(reminder, time); which was breaking the way the items were 
+        // being stored in local storage.. 
+        localStorage.setItem(time, reminder);
+
+    })
+
 })
